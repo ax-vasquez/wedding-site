af863a6dda783e508a309d6d7dca61da8867ca46
Armando Vasquez <ar.xa.vasquez@gmail.com>
2024-03-03 15:30:08 -0600

    deployment related changes

--- packages/wedding-site/next.config.js
+++ packages/wedding-site/next.config.js
@@ -2,11 +2,13 @@ const path = require('path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
