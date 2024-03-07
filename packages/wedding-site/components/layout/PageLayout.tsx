81df74894ad2d852b6f822d65aa213d7ed494109
Armando Vasquez <ar.xa.vasquez@gmail.com>
2024-03-07 01:15:52 -0600

    rudimentary sign in flow using auth0 complete - only show certain sidebar items when signed in

--- packages/wedding-site/components/layout/PageLayout.tsx
+++ packages/wedding-site/components/layout/PageLayout.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Sidebar from '../sidebar/Sidebar'
 import CustomIcon from '../CustomIcon'
 import { useDispatch } from 'react-redux'
 import { toggleShowSidebar } from '@/redux/sidebarSlice'
 import Head from 'next/head'
-import { UserProvider } from '@auth0/nextjs-auth0/client'
+import { useUser } from '@auth0/nextjs-auth0/client'
 
 interface PageLayoutProps {
     pageTitle: string
@@ -16,44 +16,41 @@ const PageLayout: React.FC<PageLayoutProps> = ({
     children
 }) => {
 
-    const [screen, setScreen] = useState(null)
-
+    const user = useUser()
     const dispatch = useDispatch()
 
     return (
         <div className="h-full">
-            <UserProvider>
-                <Head>
-                    <title>{`L & M Wedding | ${pageTitle}`}</title>
-                </Head>
-                <Sidebar />
-                <div className='fixed top-5 left-5 z-20 rounded-lg hover:cursor-pointer'
-                    onClick={() => dispatch(toggleShowSidebar())}
-                >
-                    <CustomIcon
-                        id="sidebar-menu-button"
-                        fileName="bootstrap-list"
-                        height={48}
-                        width={48}
-                        className="text-white"
-                    />
-                </div>
-                <a className='fixed top-5 right-5 z-20 hover:cursor-pointer inline-flex items-center'
-                    href='/api/auth/login'
-                >
-                    <CustomIcon
-                        id="sidebar-menu-button"
-                        fileName="bootstrap-people-circle"
-                        height={48}
-                        width={48}
-                        className="text-white"
-                    />
-                    <span className='text-2xl text-white ml-4'>{screen ? `Some User` : `Sign In`}</span>
-                </a>
-                <main>
-                    {children}
-                </main>
-            </UserProvider>
+            <Head>
+                <title>{`L & M Wedding | ${pageTitle}`}</title>
+            </Head>
+            <Sidebar />
+            <div className='fixed top-5 left-5 z-20 rounded-lg hover:cursor-pointer'
+                onClick={() => dispatch(toggleShowSidebar())}
+            >
+                <CustomIcon
+                    id="sidebar-menu-button"
+                    fileName="bootstrap-list"
+                    height={48}
+                    width={48}
+                    className="text-white"
+                />
+            </div>
+            <a className='fixed top-5 right-5 z-20 hover:cursor-pointer inline-flex items-center'
+                href={user.user ? '/api/auth/logout': '/api/auth/login'}
+            >
+                <CustomIcon
+                    id="sidebar-menu-button"
+                    fileName="bootstrap-people-circle"
+                    height={48}
+                    width={48}
+                    className="text-white"
+                />
+                <span className='text-2xl text-white ml-4'>{user.user ? user.user.email : `Sign In`}</span>
+            </a>
+            <main>
+                {children}
+            </main>
         </div>
     )
 }
