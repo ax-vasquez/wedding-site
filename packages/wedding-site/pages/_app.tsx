ed11294ce6a8b39692decbc2ef9b4f5528928c34
Armando Vasquez <mando@mcnuggies.dev>
2024-09-19 02:45:08 -0500

    some cleanup

--- packages/wedding-site/pages/_app.tsx
+++ packages/wedding-site/pages/_app.tsx
@@ -1,6 +1,5 @@
 import '@/styles/main.scss'
 import '@/styles/globals.css'
-import axios from 'axios';
 import type { AppProps } from 'next/app'
 
 import { Provider } from 'react-redux'
@@ -8,7 +7,6 @@ import store from '../redux/store'
 import { CookiesProvider } from 'react-cookie';
  
 export default function MyApp({ Component, pageProps }: AppProps) {
-  // axios.defaults.headers.common["Content-Type"] = "application/json"
   return (
     <CookiesProvider>
       <Provider store={store}>
