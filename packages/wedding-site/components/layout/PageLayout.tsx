ae47e52e2f4730b34168539dc3374e6b071d6148
Armando Vasquez <mando@mcnuggies.dev>
2024-09-22 15:23:08 -0500

    Force Login/Registration form to show when not signed in (#26)
    * surface actual errors to users
    
    * lockout site access if user not signed in (front end only - API has separate protections)
    
    * redirect to /rsvp on login/signup; redirect to home when not signed in

--- packages/wedding-site/components/layout/PageLayout.tsx
+++ packages/wedding-site/components/layout/PageLayout.tsx
@@ -5,8 +5,8 @@ import { useDispatch } from 'react-redux'
 import { toggleShowSidebar } from '@/redux/sidebarSlice'
 import Head from 'next/head'
 import { AuthModal, UserClaims } from '../modal/AuthModal'
-import { useCookies } from 'react-cookie'
 import { useUser } from '@/hooks/useUser'
+import { useRouter } from 'next/navigation'
 
 interface PageLayoutProps extends PropsWithChildren {
     pageTitle: string
@@ -17,9 +17,19 @@ const PageLayout: React.FC<PageLayoutProps> = ({
     children
 }) => {
 
+    const router = useRouter()
     const user = useUser()
     const [showAuthModal, setShowAuthModal] = useState(false)
     const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (!user) {
+            setShowAuthModal(true)
+            router.push('/')
+        } else {
+            setShowAuthModal(false)
+        }
+    }, [user])
 
     return (
         <div className="h-full">
